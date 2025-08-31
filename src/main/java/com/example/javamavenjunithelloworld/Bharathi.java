7c529903bcc8a6681e3996d1c79c2ccea78a07ba
satyabharathi19 <148929278+satyabharathi19@users.noreply.github.com>
2025-08-31 20:43:07 +0530

    BharathiM0001 wrote coe for finding area of circle

--- src/main/java/com/example/javamavenjunithelloworld/Bharathi.java
+++ src/main/java/com/example/javamavenjunithelloworld/Bharathi.java
@@ -1,4 +1,4 @@
-
+package com.example.javamavenjunithelloworld;
 public class Bharathi {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunithelloworld;
+public class Utility {
+
+    public static double areaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void satya() {
+        System.out.println("I am Bharathi");
+    }
+}
